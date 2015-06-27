a2a79f10b74ca9d06754b08c58de23a398ae832f
zamoramm <zamoramm@gmail.com>
2015-06-27 12:27:28 -0700

    Added MVC functionality to the website.

--- DeepThinkLabBlogEngine/src/DeepThinkLabBlogEngine/Startup.cs
+++ DeepThinkLabBlogEngine/src/DeepThinkLabBlogEngine/Startup.cs
@@ -1,10 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNet.Builder;
+﻿using Microsoft.AspNet.Builder;
 using Microsoft.AspNet.Http;
 using Microsoft.Framework.DependencyInjection;
+
 
 namespace DeepThinkLabBlogEngine
 {
@@ -13,13 +10,18 @@ namespace DeepThinkLabBlogEngine
         // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
+
         }
 
         public void Configure(IApplicationBuilder app)
         {
-            app.Run(async (context) =>
+            app.UseMvc(routes =>
             {
-                await context.Response.WriteAsync("Hello World!");
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller}/{action}/{id?}",
+                    defaults: new { controller = "Home", action = "Index" });
             });
         }
     }
