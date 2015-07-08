a19afc750e12973583bf167171ac7989b711cac2
zamoramm <zamoramm@gmail.com>
2015-07-07 19:21:51 -0700

    Added gulp and tasks to clean and copy js files to wwwroot

---
+++ DeepThinkLabBlogEngine/src/DeepThinkLabBlogEngine/Gulpfile.js
@@ -0,0 +1,28 @@
+﻿/// <binding BeforeBuild='copy' Clean='clean' />
+var gulp = require('gulp'),
+    del = require('del'),
+    fs = require("fs");
+
+eval("var project = " + fs.readFileSync("./project.json"));
+
+var paths = {
+    bower: "./bower_components/",
+    lib: "./" + project.webroot + "/lib/"
+};
+
+gulp.task('copy', ['clean'], function () {
+    var bower = {
+        "bootstrap": "bootstrap/dist/**/*.{js,map,css,ttf,svg,woff,eot}"
+    }
+
+    for (var destinationDir in bower) {
+        console.log("Source: " + paths.bower + bower[destinationDir]);
+        console.log("Destination: " + paths.lib + destinationDir);
+        gulp.src(paths.bower + bower[destinationDir])
+      .pipe(gulp.dest(paths.lib + destinationDir));
+    }
+});
+
+gulp.task('clean', function (cb) {
+    del([paths.lib], cb);
+});
